2b6eb153d26bfb761309261b6d2496414e6403c1
Robin Munn <rmunn@pobox.com>
2020-01-28 16:02:37 +0700

    Bump version to 0.1.8
    ## [0.1.8] - 2020-01-28
    
    Attempting to give GitHub time to catch up and realize the v#.#.# tag has been pushed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.8] - 2020-01-28
+
+Attempting to give GitHub time to catch up and realize the v#.#.# tag has been pushed
+
 ## [0.1.7] - 2020-01-28
 
 This should become version 0.1.7.
@@ -68,7 +72,7 @@ First release
 ### Added
 - Workflow code to show extra info
 
-[Unreleased]: https://github.com/rmunn/Testing/compare/v0.1.6...HEAD
+[Unreleased]: https://github.com/rmunn/Testing/compare/v0.1.7...HEAD
 [0.0.1]: https://github.com/rmunn/Testing/releases/tag/v0.0.1
 [0.0.2]: https://github.com/rmunn/Testing/compare/v0.0.1...v0.0.2
 [0.1.0]: https://github.com/rmunn/Testing/compare/v0.0.2...v0.1.0
@@ -80,3 +84,4 @@ First release
 [0.1.5]: https://github.com/rmunn/Testing/compare/v0.1.4...v0.1.5
 [0.1.6]: https://github.com/rmunn/Testing/compare/v0.1.5...v0.1.6
 [0.1.7]: https://github.com/rmunn/Testing/compare/v0.1.6...v0.1.7
+[0.1.8]: https://github.com/rmunn/Testing/compare/v0.1.7...v0.1.8
--- src/Testing/AssemblyInfo.fs
+++ src/Testing/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Testing")>]
 [<assembly: AssemblyProductAttribute("Testing")>]
-[<assembly: AssemblyVersionAttribute("0.1.7")>]
+[<assembly: AssemblyVersionAttribute("0.1.8")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2020-01-28T00:00:00.0000000+07:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.7")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.7")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.8")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.8")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","4fa7cf037d4482fa180fdadcfb81667c1f8b8df9")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","1d745e7979208893804100508a2c73b2d2404cbf")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Testing"
     let [<Literal>] AssemblyProduct = "Testing"
-    let [<Literal>] AssemblyVersion = "0.1.7"
+    let [<Literal>] AssemblyVersion = "0.1.8"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2020-01-28T00:00:00.0000000+07:00"
-    let [<Literal>] AssemblyFileVersion = "0.1.7"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.7"
+    let [<Literal>] AssemblyFileVersion = "0.1.8"
+    let [<Literal>] AssemblyInformationalVersion = "0.1.8"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "4fa7cf037d4482fa180fdadcfb81667c1f8b8df9"
+    let [<Literal>] AssemblyMetadata_GitHash = "1d745e7979208893804100508a2c73b2d2404cbf"
